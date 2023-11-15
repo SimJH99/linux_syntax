5619f2211bdf036fbf5b7aca29887c6ff329dd25
SimJH99 <s99s4481@gmail.com>
2023-11-15 11:47:38 +0900

    2023-11-15 11:47

--- 7.for_steatement_pracrice3.sh
+++ 7.for_steatement_pracrice3.sh
@@ -2,7 +2,7 @@
 # mydir 폴더 생성
 # 폴더로 이동
 # 해당 폴더에서 file1~file100까지 100개의 파일을 만들고,
-# 해당 파일마다 각각 hello world form file1, hello... 문구 입력
+# 해당 파일마다 각각 hello world form file1, hello... 문구를 100번 입력
 # practice completed 출력
 echo for statements practice3 start
 mkdir mydir
@@ -10,6 +10,9 @@ cd mydir
 for a in {1..100}
 do
     touch file$a.txt
+    for b in {1..100}
+    do
     echo "hello world form file$a" >> file$a.txt
+done
 done
 echo practice completed 출력
