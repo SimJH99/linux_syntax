0516c4084e7afd8eefd36003253cb65f29367fc2
SimJH99 <s99s4481@gmail.com>
2023-11-15 13:53:04 +0900

    2023-11-15 13:52

--- 4.for_statements_practice.sh
+++ 4.for_statements_practice.sh
@@ -1,3 +1,5 @@
+# for문을 이용하여 hello world1, helloworld2 ~ 100까지 문구가 나오도록 출력
+]
 for a in {1..100}
     do
         echo "hello world$a"
--- 5.for_steatment_practice.sh
+++ 5.for_steatment_practice.sh
@@ -1,3 +1,8 @@
+# for문을 이용하여 hello world1, helloworld2등 100까지 문구가 적힌 파일 생성
+# -mydir2디렉토리 생성
+# -해당 디렉토리에서 파일 생성
+# -파일안에 helloworld~helloworld100까지 생성
+# -한번 실행한 후, 재실행 했을 때, 오류가 없게 처리하라.
 echo start
 
 if [ -d "mydir2" ]
@@ -8,7 +13,7 @@ else
     cd mydir2
 fi 
 
-if [ -f "text.txt" ]
+if [ -d "text.txt" ]
 then
     rm -rf text.txt
     touch text.txt
@@ -17,8 +22,8 @@ else
 fi
 
 for a in {1..100}
-do 
-    echo "hello world$a" >> text.txt
+    do 
+        echo "hello world$a" >> text.txt
 done
 
 echo finish
